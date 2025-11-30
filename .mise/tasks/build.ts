17f8cfdb9c05603588d5422a8a0f36d5cca617f9
Mark Malstrom <mark@malstrom.me>
2025-11-30 11:45:49 -0600

    Add publish action

--- .mise/tasks/build.ts
+++ .mise/tasks/build.ts
@@ -2,6 +2,10 @@
 //MISE description="Build the library"
 //MISE outputs = ["dist"]
 
+import { rm } from "node:fs/promises";
+import { $ } from "bun";
 import { build } from "./build/build.ts";
 
+await rm("dist");
+await $`mise run check`;
 await build();
